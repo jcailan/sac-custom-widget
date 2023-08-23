39faacd8699e273abb2d8fee042260c533ceb78d
Jhodel Cailan <jhodel.cailan@gmail.com>
2023-08-23 20:02:39 +0800

    Fixed SAP Horizon theme issue

--- src/components/chart/sankey/styling.js
+++ src/components/chart/sankey/styling.js
@@ -1,74 +1,65 @@
-(function() {
-	// require("@ui5/webcomponents/dist/Assets.js");
-	// require("@ui5/webcomponents-fiori/dist/Assets.js");
-	// const { setTheme } = require("@ui5/webcomponents-base/dist/config/Theme.js");
-	// setTheme("sap_horizon");
+import "@ui5/webcomponents/dist/Assets.js";
+import "@ui5/webcomponents-fiori/dist/Assets.js";
+import { setTheme } from "@ui5/webcomponents-base/dist/config/Theme.js";
+setTheme("sap_horizon");
 
-	require("@ui5/webcomponents/dist/CheckBox");
-	require("@ui5/webcomponents/dist/Button");
-	require("@ui5/webcomponents/dist/ColorPalette.js");
+import "@ui5/webcomponents/dist/CheckBox";
+import "@ui5/webcomponents/dist/Button";
+import "@ui5/webcomponents/dist/ColorPalette.js";
 
-	const template = document.createElement('template');
-	template.innerHTML = `
-	<style>
-	#root {
-		margin: 0.5rem;
-		/* display: flex;
-		flex-direction: column; */
-	}
-	</style>
+const template = document.createElement('template');
+template.innerHTML = `
 	<div id="root">
 		<ui5-checkbox id="isTotalIncluded" text="Include Total"></ui5-checkbox>
 		<br />
 		<ui5-color-palette>
-			<ui5-color-palette-item value="darkblue" class="ui5-content-density-compact"></ui5-color-palette-item>
+			<ui5-color-palette-item value="darkblue"></ui5-color-palette-item>
 		</ui5-color-palette>
 		<br />
-		<ui5-button id="apply" class="ui5-content-density-compact">Apply</ui5-button>
+		<ui5-button id="apply">Apply</ui5-button>
 	</div>
-	`;
+`;
 
-	class Styling extends HTMLElement {
-		constructor() {
-			super();
+class Styling extends HTMLElement {
+	constructor() {
+		super();
 
-			this._shadowRoot = this.attachShadow({ mode: 'open' });
-			this._shadowRoot.appendChild(template.content.cloneNode(true));
-			this._root = this._shadowRoot.getElementById('root');
+		this._shadowRoot = this.attachShadow({ mode: 'open' });
+		this._shadowRoot.appendChild(template.content.cloneNode(true));
+		this._root = this._shadowRoot.getElementById('root');
 
-			this._apply = this._shadowRoot.getElementById('apply');
-			this._apply.addEventListener('click', () => {
-				const isTotalIncluded = this._shadowRoot.getElementById('isTotalIncluded').checked;
-				this.dispatchEvent(new CustomEvent('propertiesChanged', { detail: { properties: { isTotalIncluded } } }));
-			});
-		}
+		this._apply = this._shadowRoot.getElementById('apply');
+		this._apply.addEventListener('click', () => {
+			const isTotalIncluded = this._shadowRoot.getElementById('isTotalIncluded').checked;
+			this.dispatchEvent(new CustomEvent('propertiesChanged', { detail: { properties: { isTotalIncluded } } }));
+		});
+	}
 
-		// ------------------
-		// LifecycleCallbacks
-		// ------------------
-		async onCustomWidgetBeforeUpdate(changedProps) {
-		}
+	// ------------------
+	// LifecycleCallbacks
+	// ------------------
+	async onCustomWidgetBeforeUpdate(changedProps) {
+	}
 
-		async onCustomWidgetAfterUpdate(properties) {
-			if (properties.isTotalIncluded !== undefined) {
-				this._shadowRoot.getElementById('isTotalIncluded').checked = properties.isTotalIncluded;
-			}
-		}
-
-		async onCustomWidgetResize(width, height) {
-		}
-
-		async onCustomWidgetDestroy() {
-			this.dispose();
-		}
-
-		// ------------------
-		//
-		// ------------------
-
-		dispose() {
+	async onCustomWidgetAfterUpdate(properties) {
+		if (properties.isTotalIncluded !== undefined) {
+			this._shadowRoot.getElementById('isTotalIncluded').checked = properties.isTotalIncluded;
 		}
 	}
 
-	customElements.define('rizing-sankey-chart-styling', Styling);
-})();
+	async onCustomWidgetResize(width, height) {
+	}
+
+	async onCustomWidgetDestroy() {
+		this.dispose();
+	}
+
+	// ------------------
+	//
+	// ------------------
+
+	dispose() {
+	}
+}
+
+customElements.define('rizing-sankey-chart-styling', Styling);
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,4 @@
-import "@ui5/webcomponents/dist/Assets.js";
-import "@ui5/webcomponents-fiori/dist/Assets.js";
-import { setTheme } from "@ui5/webcomponents-base/dist/config/Theme.js";
-setTheme("sap_horizon");
-
 import "@ui5/webcomponents-fiori/dist/DynamicSideContent";
-// import "@ui5/webcomponents/dist/CheckBox";
-// import "@ui5/webcomponents/dist/Button";
-// import "@ui5/webcomponents/dist/ColorPalette.js";
 
 import "./components/BaseComponent";
 import "./components/chart/sankey/main";
