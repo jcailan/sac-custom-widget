084bc84f7b9bfe5e6264efb270b94783ecff5f33
Jhodel Cailan <jhodel.cailan@gmail.com>
2023-08-28 12:39:44 +0800

    Fixed propagation of changed properties

--- sankey-chart/src/index.ts
+++ sankey-chart/src/index.ts
@@ -11,10 +11,14 @@ import { twoDimensions as data } from "./data/products";
 window.addEventListener("load", () => {
 	const main = document.getElementById("main") as BaseComponent;
 	main.dataBinding = data;
-	main.onCustomWidgetAfterUpdate({});
+	main.onCustomWidgetAfterUpdate(main);
 
 	const styling = document.getElementById("styling") as BaseComponent;
 	styling.addEventListener("propertiesChanged", event => {
-		main.onCustomWidgetAfterUpdate(event.detail.properties);
+		const { properties } = event.detail;
+		for (const property in properties) {
+			main[property] = properties[property];
+		}
+		main.onCustomWidgetAfterUpdate(main);
 	});
 });
